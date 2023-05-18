da28681860a30b354278d73321c9958ae7e6def3
sdbarlow <sethdbarlow@gmail.com>
2023-05-18 10:33:12 -0500

    added password null

---
+++ migrations/versions/cbe8482a2d51_password_can_be_null.py
@@ -0,0 +1,24 @@
+"""password can be null
+
+Revision ID: cbe8482a2d51
+Revises: 259cd458a673
+Create Date: 2023-05-18 10:32:48.907828
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cbe8482a2d51'
+down_revision = '259cd458a673'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
