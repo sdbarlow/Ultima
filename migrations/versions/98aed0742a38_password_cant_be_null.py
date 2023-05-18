a18266dee1c52a0b76c47e51a7276b72f2a259a3
sdbarlow <sethdbarlow@gmail.com>
2023-05-18 10:46:07 -0500

    password rolled back

---
+++ migrations/versions/98aed0742a38_password_cant_be_null.py
@@ -0,0 +1,24 @@
+"""password cant be null
+
+Revision ID: 98aed0742a38
+Revises: cbe8482a2d51
+Create Date: 2023-05-18 10:45:39.290762
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '98aed0742a38'
+down_revision = 'cbe8482a2d51'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
